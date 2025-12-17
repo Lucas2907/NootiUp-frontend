e74efe394d02b8a96acdd39242add6f0b68e84f0
Lucas Garcia <lucaspasagarcia29@gmail.com>
2025-12-17 20:38:17 +0000

    create single component for login/register

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -1,11 +1,20 @@
 import hidePassword from "../../assets/images/Hide-password.png";
 
-function Main() {
+type MainProps = {
+    title: string,
+    description: string,
+    route: "signin" | "signup"
+}
+
+function Main({ title, description, route }: MainProps) {
+
+    const isSignin = route === "signin"
+
     return (
         <div className="main">
-            <h1 className="main__title">Welcome Back!</h1>
+            <h1 className="main__title">{title}</h1>
             <p className="main__description">
-                Aprender é revigorante e ter você aqui novamente nos revigora mais ainda
+                {description}
             </p>
             <form className="form">
                 <div className="form__inputs">
@@ -20,12 +29,33 @@ function Main() {
                             alt="icone de senha Visivel"
                         />
                     </div>
+                    {!isSignin && (
+                        <div className="form__field form__password form__password-confirmation">
+                            <input className="form__input" type="text" placeholder="Confirm Password" />
+                            <img
+                                className="form__icon"
+                                src={hidePassword}
+                                alt="icone de senha Visivel"
+                            />
+                        </div>
+                    )}
                 </div>
-                <p className="form__link-password">Forgot Password?</p>
-                <button className="form__submit">Login</button>
+                {isSignin && (
+                    <p className="form__link-password">Forgot Password?</p>)}
+                <button className="form__submit">{isSignin ? "Login" : "Register"}</button>
             </form>
             <p className="main__link">
-                Not a member? <span className="main__link_redirect">Register now</span>
+                {isSignin ? (
+                    <>
+                        Not a member?{" "}
+                        <span className="main__link_redirect">Register Now</span>
+                    </>
+                ) : (
+                    <>
+                        A member?{" "}
+                        <span className="main__link_redirect">Login Now</span>
+                    </>
+                )}
             </p>
         </div>
     );
