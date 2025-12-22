9a0bda3401e4b17217e0779be9210cb3b8698374
Lucas Garcia <lucaspasagarcia29@gmail.com>
2025-12-22 22:54:13 +0000

    change redirectComponente according to route

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,13 +1,31 @@
 import Logo from "../Logo/Logo"
 import RedirectButton from "../RedirectButton/RedirectButton"
+import { Link } from "react-router-dom"
 
 
+type HeaderProps = {
+    route: "signin" | "signup"
+}
 
-function Header({ text }: { text: string }) {
+
+function Header({ route }: HeaderProps) {
+
+    const isSignin = route === "signin"
+
     return (
         <div className="header">
             <Logo />
-            <RedirectButton text={text} />
+            {isSignin ? (
+                <>
+                    <Link className="app-link" to={"/signup"}><RedirectButton text={"Registrar"} /></Link>
+                </>
+            ) : (
+                <>
+                    <Link className="app-link" to={"/signin"}><RedirectButton text={"Entrar"} /></Link>
+                </>
+            )}
+
+
         </div>
 
     )
