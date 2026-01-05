5ba263547a8c2cc8addf58a534ec48d4476b27ab
Lucas Garcia <lucaspasagarcia29@gmail.com>
2026-01-05 16:24:25 +0000

    create and test preloader

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -6,11 +6,13 @@ import MainPage from "../MainPage/MainPage"
 import MyProgress from "../MyProgress/MyProgress"
 import Layout from "../Layout/Layout"
 import MyProfile from "../MyProfile/MyProfile"
+import Preloader from "../Preloader/Preloader"
 
 function App() {
   return (
     <Routes>
       <Route element={<Layout />}>
+        <Route path="/preloader" element={<Preloader />} />
         <Route path="/signin" element={<Main route="signin" />} />
         <Route path="/signup" element={<Main route="signup" />} />
         <Route path="/" element={<MainPage />} />
