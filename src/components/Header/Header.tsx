3493e3979dbf9b5c25e274b717e204e39ee90a85
Lucas Garcia <lucaspasagarcia29@gmail.com>
2026-01-31 13:34:03 +0000

    create a burguer menu to mobile devices

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,44 +1,108 @@
 import Logo from "../Logo/Logo"
 import RedirectButton from "../RedirectButton/RedirectButton"
 import { Link, useLocation, NavLink } from "react-router-dom"
+import { useState } from "react"
 import optionsIcon from "../../assets/images/three-lines.png"
+
 function Header() {
     const { pathname } = useLocation()
-    const customClassName = ({ isActive }: { isActive: boolean }) => "header__list-item app-link " + (isActive ? "header__list-item_active " : "")
+    const [isMenuOpen, setIsMenuOpen] = useState(false)
+
+    const customClassName = ({ isActive }: { isActive: boolean }) =>
+        "header__list-item app-link " + (isActive ? "header__list-item_active " : "")
+
+    const customClassNameMobile = ({ isActive }: { isActive: boolean }) =>
+        "header__list-item-mobile app-link " + (isActive ? "header__list-item-mobile_active " : "")
 
     const isSignin = pathname === "/signin"
     const isSignup = pathname === "/signup"
-
-
-
 
     if (isSignin || isSignup) {
         return (
             <header className="header">
                 <Logo />
                 {isSignin && (
-                    <Link to={"/signup"} className="app-link"><RedirectButton text={"Registrar"} isLogged={false} /></Link>
-                )
-                }
+                    <Link to="/signup" className="app-link">
+                        <RedirectButton text="Registrar" isLogged={false} />
+                    </Link>
+                )}
                 {isSignup && (
-                    <Link to={"/signin"} className="app-link"><RedirectButton text={"Entrar"} isLogged={false} /></Link>
-                )
-                }
-            </header >
+                    <Link to="/signin" className="app-link">
+                        <RedirectButton text="Entrar" isLogged={false} />
+                    </Link>
+                )}
+            </header>
         )
-
     }
+
+    const toggleMenu = () => setIsMenuOpen(!isMenuOpen)
+
     return (
         <header className="header">
-            <Link className="app-link" to={"/"}><Logo /></Link>
+            <Link className="app-link" to="/">
+                <Logo />
+            </Link>
+
+
             <nav className="header__list">
-                <NavLink className={customClassName} to={"/"}>Página Inicial</NavLink>
-                <NavLink className={customClassName} to={"/my-progress"}>Meu Progresso</NavLink>
-                <NavLink className={customClassName} to={"/github-info"}>GitHub Info</NavLink>
-                <NavLink to={"/my-profile"} className="app-link"><RedirectButton text={'User'} isLogged={true} /></NavLink>
-            </nav >
-            <img className="header-options" src={optionsIcon} alt=" icon to show options inside aplication " />
-        </header >
+                <NavLink className={customClassName} to="/">
+                    Página Inicial
+                </NavLink>
+                <NavLink className={customClassName} to="/my-progress">
+                    Meu Progresso
+                </NavLink>
+                <NavLink className={customClassName} to="/github-info">
+                    GitHub Info
+                </NavLink>
+                <NavLink to="/my-profile" className="app-link">
+                    <RedirectButton text="User" isLogged={true} />
+                </NavLink>
+            </nav>
+
+            <NavLink
+                to="/my-profile"
+                className="app-link"
+                onClick={toggleMenu}
+            >
+                <RedirectButton text="User" isLogged={true} />
+            </NavLink>
+
+            <div className="header__mobile">
+                <img
+                    className="header-options"
+                    src={optionsIcon}
+                    alt="icon to show options inside aplication"
+                    onClick={toggleMenu}
+                />
+            </div>
+
+            {isMenuOpen && (
+                <nav className="header__mobile-menu">
+                    <NavLink
+                        className={customClassNameMobile}
+                        to="/"
+                        onClick={toggleMenu}
+                    >
+                        Página Inicial
+                    </NavLink>
+                    <NavLink
+                        className={customClassNameMobile}
+                        to="/my-progress"
+                        onClick={toggleMenu}
+                    >
+                        Meu Progresso
+                    </NavLink>
+                    <NavLink
+                        className={customClassNameMobile}
+                        to="/github-info"
+                        onClick={toggleMenu}
+                    >
+                        GitHub Info
+                    </NavLink>
+                </nav>
+
+            )}
+        </header>
     )
 }
 
